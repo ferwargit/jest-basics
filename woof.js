8e4bfc050b196ac247e0b3d8228d159d58e1d93b
Fernando Warno <ferwar@hotmail.com>
2024-12-25 20:13:16 -0300

    matchers

---
+++ woof.js
@@ -0,0 +1,8 @@
+function woof(str) {
+    if (typeof str !== 'string') {
+        return;
+    }
+    return str.length + ' woof!';
+};
+
+module.exports = woof;
---
+++ woof.test.js
@@ -0,0 +1,13 @@
+const woof = require("./woof");
+
+test("should return number of woofs", function() {
+    const result = woof("oh herro");
+    // console.log(result);
+    expect(result).toBe("8 woof!");
+    // expect(result).not.toBe("guau!");
+    // expect({ a: "one", b: "two" }).toEqual({ a: "one", b: "two" });
+    // expect(result).toMatch(/woof/i);
+    expect(['a', 'b', 'c']).toContain('a');
+    expect(['a', 'b', 'c']).toContain('b');
+    expect(['a', 'b', 'c']).toContain('c');
+});
